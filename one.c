2f1dc0935b9822af7b6d7646c6726adcd6c87f8a
sarthakJindal07 <91410321+sarthakJindal07@users.noreply.github.com>
2022-01-31 22:35:52 +0530

    Add files via upload

---
+++ one.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ int main(){
+     int a,l,b;
+    printf("enter two numbers");
+    scanf("%d %d",&l, &b);
+    a=l*b;
+    printf("area of rectoangle = %d",a);
+    return(0); 
+ }
+
+
+
+
