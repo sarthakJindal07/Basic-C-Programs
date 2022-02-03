73ec07386edab311f14fec8d682cb24a1d369682
sarthakJindal07 <91410321+sarthakJindal07@users.noreply.github.com>
2022-02-03 23:00:16 +0530

    Create variable.c

---
+++ variable.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    int currentyear;
+    int birthyear;
+    currentyear=2022;
+    birthyear=2002;
+    age = currentyear - birthyear;
+    
+    printf("my age is %d",age);
+    
+    return 0;
+}
