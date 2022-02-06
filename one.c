d3d3821fbd69778df39bf5862185b8a081e0d965
sarthakJindal07 <91410321+sarthakJindal07@users.noreply.github.com>
2022-02-06 21:52:21 +0530

    Add files via upload

--- one.c
+++ one.c
@@ -7,6 +7,12 @@
    
    name[5]='i';
    printf("my name is %s\n",name);
+
+   char food[] ="tomato";
+   printf("the best food is %s\n",food);
+
+   strcpy(food,"mulli");
+   printf("the best food is %s\n",food);
     return(0); 
  }
 //
